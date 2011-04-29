445e40a64553e211b91fc50f245f59664bae322a
Thomas Kent (Tom) <teeks99@yahoo.com>
2011-04-29 16:11:28 -0400

    Notes and start of nle video.

---
+++ Video/Clips/BigBuckBunny/CreateClips.bash
@@ -0,0 +1,12 @@
+
+# Get the high-res version
+#wget http://mirrorblender.top-ix.org/peach/bigbuckbunny_movies/big_buck_bunny_1080p_h264.mov
+
+# Get the low-res version
+#wget http://mirrorblender.top-ix.org/peach/bigbuckbunny_movies/big_buck_bunny_480p_h264.mov
+
+# Get the Audio
+#wget http://media.xiph.org/BBB/BigBuckBunny-DVDMaster-5_1-FLAC.zip
+#wget http://media.xiph.org/BBB/BigBuckBunny-stereo.flac
+
+
