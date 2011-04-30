cb5c76bd18569c20cdd7b776c92fa7387be889d1
Thomas Kent (Tom) <teeks99@yahoo.com>
2011-04-30 19:05:57 -0400

    Prep for video section

--- Video/Clips/BigBuckBunny/CreateClips.bash
+++ Video/Clips/BigBuckBunny/CreateClips.bash
@@ -10,3 +10,9 @@
 #wget http://media.xiph.org/BBB/BigBuckBunny-stereo.flac
 
 
+# Clips for compositing on one screen
+ffmpeg -i big_buck_bunny_480p_h264.mov -ss 00:05:32.583 -t 7 -vcodec mpeg4 -b 6000k -acodec libfaac -ab 192k RedSquirrel.mkv
+ffmpeg -i big_buck_bunny_480p_h264.mov -ss 00:05:40.083 -t 35.125 -vcodec mpeg4 -b 6000k -acodec libfaac -ab 192k GraySquirrel.mkv
+ffmpeg -i big_buck_bunny_480p_h264.mov -ss 00:06:18.708 -t 66.083 -vcodec mpeg4 -b 6000k -acodec libfaac -ab 192k FlyingSquirrel.mkv
+ffmpeg -i big_buck_bunny_480p_h264.mov -ss 00:04:06.583 -t 28 -vcodec mpeg4 -b 6000k -acodec libfaac -ab 192k BunnyPrep.mkv
+
