69b6ba8184b51ecc1b39d5aad210aa263ebcae00
Thomas Kent (Tom) <teeks99@yahoo.com>
2011-05-04 15:37:16 -0400

    moved to ogg/stereo file

--- Video/Clips/BigBuckBunny/CreateClips.bash
+++ Video/Clips/BigBuckBunny/CreateClips.bash
@@ -11,13 +11,13 @@
 
 
 # Intro
-ffmpeg -i big_buck_bunny_480p_h264.mov -ss 00:00:30.250 -t 17 -vcodec mpeg4 -b 6000k -acodec libfaac -ab 192k Intro.mkv
+ffmpeg -i big_buck_bunny_480p_stereo.ogg -ss 00:00:30.250 -t 17 -vcodec mpeg4 -b 6000k -acodec libfaac -ab 192k Intro.mkv
 
 # Bunny Prep
-ffmpeg -i big_buck_bunny_480p_h264.mov -ss 00:03:53.583 -t 66 -vcodec mpeg4 -b 6000k -acodec libfaac -ab 192k BunnyPrep.mkv
+ffmpeg -i big_buck_bunny_480p_stereo.ogg -ss 00:03:53.583 -t 66 -vcodec mpeg4 -b 6000k -acodec libfaac -ab 192k BunnyPrep.mkv
 
 # Clips for compositing on one screen
-ffmpeg -i big_buck_bunny_480p_h264.mov -ss 00:05:32.583 -t 7 -vcodec mpeg4 -b 6000k -acodec libfaac -ab 192k RedSquirrel.mkv
-ffmpeg -i big_buck_bunny_480p_h264.mov -ss 00:05:40.083 -t 35.125 -vcodec mpeg4 -b 6000k -acodec libfaac -ab 192k GraySquirrel.mkv
-ffmpeg -i big_buck_bunny_480p_h264.mov -ss 00:06:18.708 -t 66.083 -vcodec mpeg4 -b 6000k -acodec libfaac -ab 192k FlyingSquirrel.mkv
+ffmpeg -i big_buck_bunny_480p_stereo.ogg -ss 00:05:32.583 -t 7 -vcodec mpeg4 -b 6000k -acodec libfaac -ab 192k RedSquirrel.mkv
+ffmpeg -i big_buck_bunny_480p_stereo.ogg -ss 00:05:40.083 -t 35.125 -vcodec mpeg4 -b 6000k -acodec libfaac -ab 192k GraySquirrel.mkv
+ffmpeg -i big_buck_bunny_480p_stereo.ogg -ss 00:06:18.708 -t 66.083 -vcodec mpeg4 -b 6000k -acodec libfaac -ab 192k FlyingSquirrel.mkv
 
