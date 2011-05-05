00c9419f266cb3ca1dd41d4d278f6ab2fabf9ae1
Thomas Kent (Tom) <teeks99@yahoo.com>
2011-05-05 17:58:17 -0400

    Kdenlive tutorial mostly complete

--- Video/Clips/BigBuckBunny/CreateClips.bash
+++ Video/Clips/BigBuckBunny/CreateClips.bash
@@ -15,11 +15,15 @@
 # Intro
 ffmpeg -i big_buck_bunny_480p_stereo.ogg -ss 00:00:30.250 -t 17 -vcodec mpeg4 -b 6000k -acodec libfaac -ab 192k Intro.mkv
 
-# Bunny Prep
+# Bunny
 ffmpeg -i big_buck_bunny_480p_stereo.ogg -ss 00:03:53.583 -t 66 -vcodec mpeg4 -b 6000k -acodec libfaac -ab 192k BunnyPrep.mkv
+ffmpeg -i big_buck_bunny_480p_stereo.ogg -ss 00:07:52.875 -t 7.25 -vcodec mpeg4 -b 6000k -acodec libfaac -ab 192k BunnyRelax.mkv
+
 
 # Clips for compositing on one screen
-ffmpeg -i big_buck_bunny_480p_stereo.ogg -ss 00:05:32.583 -t 7 -vcodec mpeg4 -b 6000k -acodec libfaac -ab 192k RedSquirrel.mkv
-ffmpeg -i big_buck_bunny_480p_stereo.ogg -ss 00:05:40.083 -t 35.125 -vcodec mpeg4 -b 6000k -acodec libfaac -ab 192k GraySquirrel.mkv
-ffmpeg -i big_buck_bunny_480p_stereo.ogg -ss 00:06:18.708 -t 66.083 -vcodec mpeg4 -b 6000k -acodec libfaac -ab 192k FlyingSquirrel.mkv
+ffmpeg -i big_buck_bunny_480p_stereo.ogg -ss 00:05:32.583 -t 5 -vcodec mpeg4 -b 6000k -acodec libfaac -ab 192k RedSquirrel.mkv
+ffmpeg -i big_buck_bunny_480p_stereo.ogg -ss 00:05:40.083 -t 10 -vcodec mpeg4 -b 6000k -acodec libfaac -ab 192k GraySquirrel.mkv
+ffmpeg -i big_buck_bunny_480p_stereo.ogg -ss 00:06:31.208 -t 15 -vcodec mpeg4 -b 6000k -acodec libfaac -ab 192k FlyingSquirrel.mkv
+
+
 
