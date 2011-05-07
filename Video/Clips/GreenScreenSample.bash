3bf99c12a62ddefa7f03c14a6503e06025bf0656
Thomas Kent (Tom) <teeks99@yahoo.com>
2011-05-07 15:24:43 -0400

    Getting everything set.

---
+++ Video/Clips/GreenScreenSample.bash
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#I haven't yet been able to fine a Green Screen example clip with a good license, so this is what I'm using for the first semester:
+#http://vimeo.com/3886441
+
+# Background
+# http://www.archive.org/details/CEPriceWinterClouds - CC0
+wget http://www.archive.org/download/CEPriceWinterClouds/winter_clouds.mpg
